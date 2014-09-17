9895beb470ccb36d2ffaac4dd9a3943e7bbda231
eve-link <eve.link.1992@gmail.com>
2014-09-17 12:44:40 +0300

    Rename welcome controller to posts

---
+++ views/tags/tags_index.php
@@ -0,0 +1,18 @@
+<?foreach( $posts as $post):?>
+    <div class="span8">
+        <h1><a href="<?= BASE_URL ?>posts/view/<?= $post['post_id'] ?>">
+                <?=$post['post_subject']?>
+            </a>
+        </h1>
+        <p><?=$post['post_text']?></p>
+        <div>
+            <span class="badge badge-success"><?=$post["post_created"]?></span>
+            <div class="pull-right">
+                <?foreach ($tags[$post['post_id']] as $tag):?>
+                    <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a>
+                <?endforeach?>
+                <span class="label">alice</span>
+            </div>
+        </div>
+    </div>
+<? endforeach ?>
